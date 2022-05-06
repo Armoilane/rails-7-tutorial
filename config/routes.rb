3318e82a43cd8e9fab69eb61c9cccf2bc43bce82
Ari Moilanen <armo@iki.fi>
2022-05-06 16:51:51 +0300

    Delete test routes. Add route to about.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
+
   get 'static_pages/home'
   get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
 
   # Defines the root path route ("/")
   root "application#hello"
