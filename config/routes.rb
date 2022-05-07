5ba49e84595b3b26db59e470abb9eadfd35d4d53
Ari Moilanen <armo@iki.fi>
2022-05-07 16:30:22 +0300

    Add route and test for Home as root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-
-  # Defines the root path route ("/")
-  root "application#hello"
 
 end
 
