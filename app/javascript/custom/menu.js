ba78dd1da5c9b4e75571c011995d5e15f8efd839
Ari Moilanen <armo@iki.fi>
2022-05-12 17:51:13 +0300

    Add event listeners to navbar hamburger menu

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -2,11 +2,30 @@
 
 // Add toggle listeners to listen for clicks
 document.addEventListener('turbo:load', function() {
+  let hamburger = document.querySelector('#hamburger')
+
+  hamburger.addEventListener('touchstart', function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    let menu = document.querySelector('#navbar-menu')
+    menu.classList.toggle('collapse')
+  })
+
+  hamburger.addEventListener('click', function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    let menu = document.querySelector('#navbar-menu')
+    menu.classList.toggle('collapse')
+  })
+
   let account = document.querySelector('#account')
 
   account.addEventListener('click', function(event) {
     event.preventDefault()
     event.stopPropagation()
+
     let menu = document.querySelector('#dropdown-menu')
     menu.classList.toggle('active')
   })
