f9cad62249fd33e83573b65cc5b793f5be4ffb00
Ari Moilanen <armo@iki.fi>
2022-05-12 18:13:05 +0300

    Add integration test for valid sign in
    Uses fixture defined earlier

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:Armo)
+  end
+
   test 'login with invalid credentials' do
     get login_path
     assert_template 'sessions/new'
@@ -13,6 +17,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     get root_path
     assert flash.empty?
+  end
+
+  test 'login with valid information' do
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
   end
 
 end
