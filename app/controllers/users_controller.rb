5b07ddc38bfdb0758ea88431cc8474204e3e208f
Ari Moilanen <armo@iki.fi>
2022-06-10 17:20:14 +0300

    Add before action filter to users controller
    Restricted to edit and update actions. Runs logged_in_user method before
    those actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -45,6 +46,15 @@ class UsersController < ApplicationController
           :email,
           :password,
           :password_confirmation)
+    end
+
+    # Before action filters
+    # Confirms a logged-in user
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url, status: :see_other
+      end
     end
 
 end
