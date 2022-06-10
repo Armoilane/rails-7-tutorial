c2a1a6f5df97e5f6aa73a64cc208711454d6b40f
Ari Moilanen <armo@iki.fi>
2022-06-10 15:53:09 +0300

    Add test for successful user edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,6 +9,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test 'unsuccessful edit' do
     get edit_user_path(@user)
     assert_template 'users/edit'
+
     patch user_path(@user), params: {
       user: {
         name: '',
@@ -17,8 +18,30 @@ class UsersEditTest < ActionDispatch::IntegrationTest
         password_confirmation: 'tat'
       }
     }
+
     assert_template 'users/edit'
     assert_select 'div.alert', text: 'The form contains 4 errors.'
   end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+
+    patch user_path(@user), params: {
+      user: {
+        name: 'Ari Moilanen',
+        email: 'armo@iki.fi',
+        password: '',
+        password_confirmation: ''
+      }
+    }
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
 end
 
