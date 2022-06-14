2ef462a86ce20fb4b4deabaa6746d3940d8f78da
Ari Moilanen <armo@iki.fi>
2022-06-14 16:34:40 +0300

    Add activation token and digest to user model
    Using method reference with before_save and before_create. The creation
    of the activation token and digest are put in a private method for
    there's no need to expose those to outside users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { email.downcase! }
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   validates :name,      presence: true,
                         length: { maximum: 50 }
@@ -43,6 +44,17 @@ class User < ApplicationRecord
   def session_token
     remember_digest || remember
   end
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 
   class << self
