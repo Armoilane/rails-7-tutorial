f12217d67901d6764ca3b492eaac043f94528f72
Ari Moilanen <armo@iki.fi>
2022-06-14 16:49:23 +0300

    Simplify the downcase_email method

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,9 +48,10 @@ class User < ApplicationRecord
   private
 
     def downcase_email
-      self.email = email.downcase
+      email.downcase!
     end
 
+    # Creates and assigns the activation token and digest
     def create_activation_digest
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
