e1ad40c57d213d6dcb445d33808107fb1f08231d
Ari Moilanen <armo@iki.fi>
2022-06-19 09:39:55 +0300

    Add indexes to microposts columns
    Because the `user_id` and `created_at` are put in an array, Rails
    creates a multiple key index, which means that Active Record uses both
    keys at the same time.

--- db/migrate/20220619062901_create_microposts.rb
+++ db/migrate/20220619062901_create_microposts.rb
@@ -1,4 +1,5 @@
 class CreateMicroposts < ActiveRecord::Migration[7.0]
+
   def change
     create_table :microposts do |t|
       t.text :content
@@ -6,5 +7,9 @@ class CreateMicroposts < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+
+    add_index :microposts, [:user_id, :created_at]
   end
+
 end
+
