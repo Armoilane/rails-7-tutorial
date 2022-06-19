51b96b49518a6e173325ae3c991b6f0af1268395
Ari Moilanen <armo@iki.fi>
2022-06-19 09:53:48 +0300

    Add first micropost tests
    Validates the micropost with and without an user id.

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:Armo)
+    # This is not idiomatically correct, will be updated later
+    @micropost = Micropost.new(content: 'Lorem ipsum', user_id: @user.id)
+  end
+
+  test 'micropost should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'user id should be present' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
 end
+
