f4985fa688d70caf661c8f0161de1401ad79be0e
Ari Moilanen <armo@iki.fi>
2022-06-21 14:31:37 +0300

    Add validations for image format and size

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,6 +5,15 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image,
+    content_type: {
+      in: %w[image/jpeg image/gif image/png],
+      message: 'Image must be of format jpeg, gif or png'
+    },
+    size: {
+      less_than: 5.megabytes,
+      message: 'Image size must be less than 5MB'
+    }
 
 end
 
