e3af9570e60ee4078d707da2c81e9237d1493f44
Ari Moilanen <armo@iki.fi>
2022-06-29 13:16:58 +0300

    Add routes to followers and following
    URLs will look like users/1/following and ../followers derived from each
    user with `:member` and since they're just pages `GET` is good enough.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,28 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get 'sessions/new'
 
   root    'static_pages#home'
-  get     '/home',      to: 'static_pages#home'
-  get     '/help',      to: 'static_pages#help'
-  get     '/about',     to: 'static_pages#about'
-  get     '/contact',   to: 'static_pages#contact'
-  get     '/signup',    to: 'users#new'
-  get     '/login',     to: 'sessions#new'
-  post    '/login',     to: 'sessions#create'
-  delete  '/logout',    to: 'sessions#destroy'
-  get     '/microposts',to: 'static_pages#home'
+  get     '/home',              to: 'static_pages#home'
+  get     '/help',              to: 'static_pages#help'
+  get     '/about',             to: 'static_pages#about'
+  get     '/contact',           to: 'static_pages#contact'
+  get     '/signup',            to: 'users#new'
+  get     '/login',             to: 'sessions#new'
+  post    '/login',             to: 'sessions#create'
+  delete  '/logout',            to: 'sessions#destroy'
+  get     '/microposts',        to: 'static_pages#home'
+  get     'password_resets/new'
+  get     'password_resets/edit'
+  get     'sessions/new'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+
 end
 
