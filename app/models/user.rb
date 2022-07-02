c166cb4802ed44a99a9607778c9fa14d3bfc7d1b
Ari Moilanen <armo@iki.fi>
2022-07-02 12:30:30 +0300

    Introduce a feed with posts from self and followed users
    `following_ids` contains the ids that the user is following.
    
    The SQL query is great and takes in the array of ids just like that.

--- app/models/user.rb
+++ app/models/user.rb
@@ -88,10 +88,9 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # Defines a proto feed
-  # Will include followed users' posts later on
+  # Returns a user's status feed
   def feed
-    Micropost.where('user_id = ?', id)
+    Micropost.where('user_id IN (?) OR user_id = ?', following_ids, id)
   end
 
   # Follows a user, other than self
